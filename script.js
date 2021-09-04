ca884fbc964117644db3d6bde5aacaa3df0d4fd1
HobbZ <zachobba@gmail.com>
2021-09-04 22:52:42 +1000

    changed arrays to strings as easier to work with, generator works for specified strings but can't add custom combos yet

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var alphabet= ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numbers=[1,2,3,4,5,6,7,8,9,0];
-var specialCharacters=["!", '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-
-//Get random function
-function getRandom(arrayInput) {
-  var i= Math.floor(Math.random() * arrayInput.length);
-  return arrayInput[i];
-}
-
 // Write password to the #password input
 function generatePassword() {
   //Asks user to enter the password length they would like
-  var passwordLength= window.prompt("Please enter the length of your desired password (max 25 characters)");
+  var passwordLength= window.prompt("Please enter the length of your desired password (min 8 characters max 125 characters)");
+  console.log(passwordLength);
 
-  for (let index = 0; index < passwordLength; index++) {
-  //Create new array that chooses an array, then chooses random element from that array
-  var getRand = [getRandom(alphabet), getRandom(numbers), getRandom(specialCharacters)];
-  //Choose random number from array.length e.g. 2 from either alphabet, numbers, specialCharacters arrays
-  var i = Math.floor(Math.random() * getRand.length);
-  var choice= getRand[i]; //display the array and chosen array index e.g. 2nd item in specified array
-  console.log(choice);
+  //Stop function if user presses cancel
+  if(!passwordLength) {
+    return;
   }
-  return;
+
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number = "1234567890";
+  var special = "!@#$%^&*()?";
+
+  var passwordcharSet= window.prompt("Type in criteria you would like to apply separated by a comma: uppercase, lowercase, number, special, all");
+  
+  //Generate password
+  var charSet = "";
+
+  if (passwordcharSet === "uppercase") {
+    charSet = alphabet;
+  }
+
+  if (passwordcharSet === "lowercase") {
+    charSet = alphabet.toLowerCase();
+  }
+
+  if (passwordcharSet === "number") {
+    charSet = number;
+  }
+
+  if (passwordcharSet === "special") {
+    charSet = special;
+  }
+
+  //adds all datatypes together
+  if (passwordcharSet === "all") {
+    charSet = alphabet+number+special+alphabet.toLowerCase();
+  }
+
+  if (passwordcharSet !== "all") {
+    //function to add features by typing
+    var passwordValue = [];
+    for (let index = 0; index < passwordcharSet.length; index++) {
+      passwordValue = passwordcharSet[index];
+      charset = charset[index];
+    }
+  }
+
+  var text="";
+  for (let index = 0; index < passwordLength; index++) {
+    var i = Math.floor(Math.random() * charSet.length);
+    text += charSet.charAt(i);
+  }
+  return text;
 }
+
+  /*if (passwordLength > 8 || passwordLength < 125) {
+    console.log(passwordLength);
+    alert("You are generating a " + passwordLength + " character password")
+    passwordCriteria();
+    return passwordLength;
+  } else if (passwordLength < 8 || passwordLength > 125 || passwordLength == null) {
+    //if password is under or over max lengths, restart function
+    alert("Insufficient password length entered, must be more than 8 characters and less than 125 characters");
+    generatePassword();
+  }*/
 
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordcharSet = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordcharSet.value = password;
 }
 
 // Add event listener to generate button
