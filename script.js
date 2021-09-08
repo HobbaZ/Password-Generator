05b0555ad323f93396e32a98d7bfd7c0c309191b
HobbZ <zachobba@gmail.com>
2021-09-08 19:47:50 +1000

    update javascript password function

--- script.js
+++ script.js
@@ -3,53 +3,64 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+
   //Asks user to enter the password length they would like
-  var passwordLength= window.prompt("Please enter the length of your desired password (min 8 characters max 125 characters)");
-  console.log(passwordLength);
+  var passwordLength= prompt("Please enter a number for the length of your desired password (min 8 characters max 128 characters)");
 
-  //Stop function if user presses cancel
-  if(!passwordLength) {
+  //if letter entered instead of number
+  if (isNaN(passwordLength)) {
+      alert("please enter numbers only")
+      return;
+    }
+
+    //if number not between specified length or null
+   if (passwordLength < 8 || passwordLength > 128) {
+    alert("invalid password length entered, password must be between 8 and 128 characters")
     return;
-  }
-
+   } 
+  
+  //options strings
   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var number = "1234567890";
-  var special = "!@#$%^&*()?";
+  var special = "!@#$%&*()?.+/<>[]|";
 
-  var passwordcharSet= window.prompt("Type in criteria you would like to apply (e.g. uns): uppercase = u, lowercase = l, number = n, special = s, all criteria = all");
+  var passwordcharSet= prompt("Type in criteria you would like to apply (e.g. uns): uppercase = u, lowercase = l, number = n, special = s, all criteria = all");
 
-  //Generate password
-
-  if(!passwordcharSet) { //uppercase default if left blank
-  var charSet = alphabet;
+  //if number entered instead of letter
+  if(!isNaN(passwordcharSet)) {
+    alert("Please enter letters only");
   } else {
-    charSet = ""; //empty string if options entered
-  }
+
+  var charSet = ""; //empty string to create new password
+  var options = ["u", "l", "n", "s", "all"];
 
   //finally figured out the selecting part!!!!
   //if prompt string includes specified part of string, add string to charSet
-  //This method works, but you can write any word containing the specified letters and it will work, will need to change for real world app
-  //can change it to full word, but I feel that is too much typing
 
-  if (passwordcharSet.includes("u")) {
+  if (passwordcharSet.includes(options[0])) {
     charSet += alphabet;
   }
 
-  if (passwordcharSet.includes("l")) {
+  if (passwordcharSet.includes(options[1])) {
     charSet += alphabet.toLowerCase();
   }
 
-  if (passwordcharSet.includes("n")) {
+  if (passwordcharSet.includes(options[2])) {
     charSet += number;
   }
 
-  if (passwordcharSet.includes("s")) {
+  if (passwordcharSet.includes(options[3])) {
     charSet += special;
   }
 
   //all adds all the strings together
-  if (passwordcharSet === "all") {
+  if (passwordcharSet === options[4]) {
   charSet += alphabet+number+special+alphabet.toLowerCase();
+  }
+
+  //If string entered doesn't match the specified strings, alert user
+  else if (!passwordcharSet.includes(options[0]) && !passwordcharSet.includes(options[1]) && !passwordcharSet.includes(options[2]) && !passwordcharSet.includes(options[3]) && !passwordcharSet.includes(options[4])) {
+    alert("invalid option entered");
   }
 
   var text="";
@@ -59,17 +70,7 @@ function generatePassword() {
   }
   return text;
 }
-
-  /*if (passwordLength > 8 || passwordLength < 125) {
-    console.log(passwordLength);
-    alert("You are generating a " + passwordLength + " character password")
-    passwordCriteria();
-    return passwordLength;
-  } else if (passwordLength < 8 || passwordLength > 125 || passwordLength == null) {
-    //if password is under or over max lengths, restart function
-    alert("Insufficient password length entered, must be more than 8 characters and less than 125 characters");
-    generatePassword();
-  }*/
+}
 
 function writePassword() {
   var password = generatePassword();
